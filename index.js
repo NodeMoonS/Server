3c2e2b322e6c958511f2cf6065582dca6ee537aa
Sergio Alferov <alferovsergio@yandex.ru>
2024-05-23 18:32:08 +0300

    fix cors

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const app = express();
 // app.use(cors());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
+  res.setHeader('Access-Control-Allow-Origin', 'https://server-g4ow.onrender.com');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Credentials', true);
