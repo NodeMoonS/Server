83f7c1ea27eb52abaf276fe84d3f36564826fd1c
Sergio Alferov <alferovsergio@yandex.ru>
2024-05-24 10:40:55 +0300

    fix cors

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const app = express();
 // app.use(cors());
-
+https://zesty-brigadeiros-212bc4.netlify.app/
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://664f6363e9e9141774111479--zesty-brigadeiros-212bc4.netlify.app');
+  res.setHeader('Access-Control-Allow-Origin', 'https://66504463d2814c02791047d5--incomparable-gaufre-fb9371.netlify.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Credentials', true);
