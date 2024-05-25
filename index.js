8292b3844ac1615386fcf537f8dd60599c0fe1c0
Sergio Alferov <alferovsergio@yandex.ru>
2024-05-25 19:10:40 +0300

    fix

--- index.js
+++ index.js
@@ -2,14 +2,11 @@ const dotenv = require('dotenv');
 dotenv.config();
 const express = require('express');
 const PORT = process.env.PORT || 5000
-const authRouter = require('./authRouter')
 const sendRouter = require('./sendRouter')
-
-const cookieParser = require('cookie-parser')
 const app = express();
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://silver-babka-247644.netlify.app');
+  res.setHeader('Access-Control-Allow-Origin', 'http://127.0.0.1:5500');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Credentials', true);
@@ -18,18 +15,11 @@ app.use((req, res, next) => {
 });
 
 
-
-app.use(cookieParser());
 app.use(express.json());
-
-app.use('/auth', authRouter)
 app.use('/send', sendRouter)
-
-const mongoose = require('mongoose');
 
 const start = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
--- sendController.js
+++ sendController.js
@@ -2,29 +2,29 @@ const nodemailer = require('nodemailer');
 class sendController {
   async contactForm(req, res) {
     const transporter = nodemailer.createTransport({
-     host: "smtp.mail.ru",
-     port: 465,
-     secure: true,
+      host: "smtp.mail.ru",
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASSWORD
       },
       tls: {
         rejectUnauthorized: false,
-    },
+      },
     });
 
-    const {email, name, surname, patronomic, goodOne, goodTwo, goodThree, goodFour, goodFive, goodSix} = req.body
+    const { email, name, surname, patronomic, goodOne, goodTwo, goodThree, goodFour, goodFive, goodSix, goodSeven, goodEight, goodNine, goodTen, goodEleven, goodTwelve, goodThirteen, goodFourteen, goodFifteen, goodSixteen, goodSeventeen, goodEighteen, goodNineteen, goodTwenty } = req.body
 
     const mailOptions = {
-      from: "ооо 'АлекСофт' <elena-ivanova-1757@mail.ru>",
-      to: "elena-ivanova-1757@mail.ru",
+      from: "ооо 'АлекСофт' <sergei.alfiorov.99@mail.ru>",
+      to: "sergei.alfiorov.99@mail.ru",
       subject: "заказ",
-      text: `Имя: ${name}\nФамилия: ${surname}\nОтчество: ${patronomic}\nEmail: ${email} \nЗаказ: ${goodOne}, ${goodTwo}, ${goodThree}, ${goodFour}, ${goodFive}, ${goodSix} \nСтатус: заказ оформлен`
+      text: `Имя: ${name}\nФамилия: ${surname}\nОтчество: ${patronomic}\nEmail: ${email} \nЗаказ: ${goodOne}, ${goodTwo}, ${goodThree}, ${goodFour}, ${goodFive}, ${goodSix}, ${goodSeven}, ${goodEight}, ${goodNine}, ${goodTen}, ${goodEleven}, ${goodTwelve}, ${goodThirteen}, ${goodFourteen}, ${goodFifteen}, ${goodSixteen}, ${goodSeventeen}, ${goodEighteen}, ${goodNineteen}, ${goodTwenty} \nСтатус: заказ оформлен`
     };
 
     try {
-       transporter.sendMail(mailOptions);
+      transporter.sendMail(mailOptions);
       res.status(200).json({ message: 'Email sent successfully' });
     } catch (error) {
       console.error(error);
