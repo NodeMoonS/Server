9681e5fe26da7d8ff02ab0d8adfd4215d19ac3d1
Sergio Alferov <alferovsergio@yandex.ru>
2024-05-28 22:56:51 +0300

    fix cors

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 
 //настройка cors
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://master--alexsoftsite.netlify.app');
+  res.setHeader('Access-Control-Allow-Origin', 'https://alexsoftsite.netlify.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.setHeader('Access-Control-Allow-Credentials', true);
